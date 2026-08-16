61bb61d1c3bcbc2d4822fcf3819c6304a71bfab7
DilaraBakayPXL <12503587@student.pxl.be>
2026-08-16 20:15:31 +0200

    projecten.js toegevoegd + verbetering op iphone scherm
    overgangen gegeven aan de projecten + "over mij" sectie proberen overzichtelijker te maken op telefoon (nu springt die wat uit)

--- script.js
+++ script.js
@@ -122,5 +122,22 @@ function showInfo(type) {
 
   document.getElementById(`btn-${type}`).classList.add('active');
 
-  document.getElementById(`info-${type}`).classList.add('visible');
+      document.getElementById(`info-${type}`).classList.add('visible');
 }
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach(function(reveal) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveal.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            reveal.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
